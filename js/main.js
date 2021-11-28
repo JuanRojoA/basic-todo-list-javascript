3d0a297b2cb8c1a69fdfa682528883df37bf491f
Juan Rojo A <jjrojoa@hotmail.com>
2021-11-27 19:17:13 -0500

    Second version. Fully responsive.

--- js/main.js
+++ js/main.js
@@ -17,87 +17,60 @@ if (localStorage.getItem("name").endsWith("s")) {
   titleElement.textContent = `${localStorage.getItem("name")}'s Todo List`;
 }
 
-if (!localStorage.getItem("todos")) {
-  todos = [];
-} else {
+if (localStorage.getItem("todos")) {
   todos = JSON.parse(localStorage.getItem("todos"));
-  console.log(localStorage.getItem("todos"));
-}
-renderTodoItem(todos);
-
-todoBtn.addEventListener("click", addItem);
-todoList.addEventListener("click", deleteCheck);
-optionSelector.addEventListener("click", filterTodo);
-
-function renderTodoItem(array) {
-  todoList.innerHTML = "";
-  for (i = 0; i < array.length; i++) {
-    const todoDiv = document.createElement("div");
-    todoDiv.classList.add("todo-div");
-    todoDiv.setAttribute("id", `${i}`);
-
-    const newTodoItem = document.createElement("li");
-    newTodoItem.innerText = array[i];
-
-    newTodoItem.classList.add("todo-item");
-    todoDiv.appendChild(newTodoItem);
-
-    const completedBtn = document.createElement("button");
-    completedBtn.innerHTML = `<span class="material-icons">task_alt</span>`;
-    completedBtn.classList.add("completed-btn");
-    todoDiv.appendChild(completedBtn);
-
-    const trashBtn = document.createElement("button");
-    trashBtn.innerHTML = `<span class="material-icons">delete_forever</span>`;
-    trashBtn.classList.add("trash-btn");
-    todoDiv.appendChild(trashBtn);
-
-    todoList.appendChild(todoDiv);
-  }
-  todoInput.value = "";
-  todoInput.focus();
-  localStorage.setItem("todos", JSON.stringify(todos));
+  renderTodoItems(todos);
+} else {
+  todos = [];
 }
 
-function addItem(e) {
+todoBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  if (todoInput.value) {
-    todos.push(todoInput.value);
-    renderTodoItem(todos);
+  if (todoInput.value != "") {
+    todos.push({ title: todoInput.value, completeState: "uncompleted" });
+    renderTodoItems(todos);
   }
-}
+  localStorage.setItem("todos", JSON.stringify(todos));
+  todoInput.focus();
+  todoInput.value = "";
+});
 
-function deleteCheck(e) {
-  const item = e.target;
-  if (item.classList[0] === "trash-btn") {
-    const itemParent = item.parentElement;
-    itemParent.remove();
+todoList.addEventListener("click", (e) => {
+  let item = e.target;
+  if (item.classList[0] === "completed-btn") {
+    let itemID = item.id[0];
+    if (todos[itemID].completeState === "completed") {
+      todos[itemID].completeState = "uncompleted";
+    } else if (todos[itemID].completeState != "completed") {
+      todos[itemID].completeState = "completed";
+    }
+    localStorage.setItem("todos", JSON.stringify(todos));
+  } else if (item.classList[0] === "trash-btn") {
     let itemID = item.id[0];
     todos.splice(itemID, 1);
     localStorage.setItem("todos", JSON.stringify(todos));
-  } else if (item.classList[0] === "completed-btn") {
     const itemParent = item.parentElement;
-    itemParent.classList.toggle("completed-task");
+    itemParent.remove();
   }
-  renderTodoItem(todos);
-}
+  renderTodoItems(todos);
+});
 
-function filterTodo(e) {
+optionSelector.addEventListener("click", (e) => {
   const todoItems = todoList.childNodes;
-  todoItems.forEach(function (todoItem) {
+  todoItems.forEach((todoItem) => {
     switch (e.target.value) {
       case "all":
         todoItem.style.display = "grid";
         break;
       case "completed":
-        if (!todoItem.classList.contains("completed-task")) {
+        if (!todoItem.classList.contains("completed")) {
           todoItem.style.display = "none";
         } else {
           todoItem.style.display = "grid";
         }
         break;
       case "uncompleted":
-        if (todoItem.classList.contains("completed-task")) {
+        if (todoItem.classList.contains("completed")) {
           todoItem.style.display = "none";
         } else {
           todoItem.style.display = "grid";
@@ -105,4 +78,20 @@ function filterTodo(e) {
         break;
     }
   });
+});
+
+function renderTodoItems(array) {
+  let todoItems = "";
+  for (i = 0; i < array.length; i++) {
+    todoItems += `<div class="todo-item ${array[i].completeState}">
+    <li class="todo">${array[i].title}</li>
+    <button class="completed-btn" id="${i}">
+      <span class="material-icons btn-icon"> task_alt </span>
+    </button>
+    <button class="trash-btn" id="${i}">
+      <span class="material-icons btn-icon"> delete_outline </span>
+    </button>
+  </div>`;
+  }
+  todoList.innerHTML = todoItems;
 }
